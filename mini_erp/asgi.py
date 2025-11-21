cb9a630e9bcf32941f9fe8d2d8782aa0bb47b286
Guille <yoerguille@gmail.com>
2025-11-21 18:08:50 +0100

    first committing my new mini-erp project

---
+++ mini_erp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mini_erp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_erp.settings')
+
+application = get_asgi_application()
