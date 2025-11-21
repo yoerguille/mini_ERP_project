3e1de7485ed84c1f933760fae4e18ce5e73afabf
Guille <yoerguille@gmail.com>
2025-11-21 19:22:38 +0100

    adding client models and admin to project

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,86 @@
 from django.contrib import admin
+from .models import Client, ClientContact, ClientFile
 
-# Register your models here.
+
+
+class ClientContactAdmin(admin.TabularInline):
+    model= ClientContact
+    extra=1
+    classes= ['collapse']
+    fields=(
+        'first_name',
+        'last_name',
+        'cargo',
+        't_number',
+        'email',
+    )
+
+class ClientFileAdmin(admin.TabularInline):
+    model=ClientFile
+    extra=1
+    classes= ['collapse']
+    fields=(
+        'file',
+        'descripcion',
+        'created_at',
+    )
+    readonly_fields= (
+        'created_at',
+    )
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "name",
+        'tipo',
+        't_number',
+        'email',
+        'direccion',
+        'other',
+        'created_at',
+    ]
+
+    search_fields=(
+        "name",
+        't_number',
+        'email',
+    )
+
+    list_filter = [
+        'tipo',
+        'created_at',
+        ]    
+    ordering = [
+        'name',
+        'tipo',
+        'created_at',
+        
+    ]
+
+    inlines= [
+        ClientContactAdmin,
+        ClientFileAdmin,
+    ]
+
+    fieldsets=(
+        'Información del Cliente', {
+            'fields' :("name",
+        'tipo',
+        't_number',
+        'email',
+        'direccion',
+        'other',
+        'created_at',)
+        }
+    ),
+    (
+        'Notas internas', {
+            'fields': (
+                'cargo',
+            ),
+            'classes':(
+                'collapse',
+            )
+        }
+    )
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Client(models.Model):
+
+    TIPOS_CLIENTE = [
+        ("club deportivo", "Club deportivo"),
+        ("colegio", "Colegio"),
+        ("ayuntamiento", "Ayuntamiento"),
+        ("universidad", "Universidad"),
+        ("empresa", "Empresa"),
+        ("particular", "Particular"),
+    ]
+
+    name = models.CharField(
+        verbose_name='Nombre',
+        max_length=255,
+    )
+
+    tipo = models.CharField(
+        verbose_name='Tipo de Cliente',
+        choices=TIPOS_CLIENTE
+    )
+
+    t_number = models.CharField(
+        verbose_name='Nº teléfono',
+        max_length=30,
+        null=True,
+        blank=True
+    )
+
+    email= models.EmailField(
+        blank=True,
+    )
+
+    direccion=models.TextField(
+        verbose_name='Direccion',
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+    other=models.TextField(
+        verbose_name='Otra información',
+        blank=True,
+        null=True,
+    )
+
+    created_at=models.DateTimeField(
+        verbose_name='Fecha de Creación',
+        default=timezone.now,
+        )
+
+    def __str__(self):
+        return self.name
+
+class ClientContact(models.Model):
+    client=models.ForeignKey(
+        'Client',
+        on_delete=models.CASCADE,
+        related_name='relacionados',
+    )
+
+    first_name = models.CharField(
+        verbose_name='Nombre',
+        max_length=255,
+    )
+
+    last_name =models.CharField(
+        verbose_name='Apellidos',
+        max_length=255,
+    )
+
+    cargo = models.CharField(
+        verbose_name='Cargo',
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+
+    t_number = models.CharField(
+        verbose_name='Nº teléfono',
+        max_length=30,
+        null=True,
+        blank=True
+    )
+
+    email= models.EmailField(
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.first_name} ({self.client.name})"
+    
+
+class ClientFile(models.Model):
+    client=models.ForeignKey(
+        'Client',
+        on_delete=models.CASCADE,
+        related_name='files',
+    )
+
+    file = models.FileField(
+        verbose_name="Archivos",
+        upload_to="clients/",
+        null=True,
+        blank=True,
+    )
+
+    descripcion = models.TextField(
+        verbose_name='Descripcion de archivo',
+        null=True,
+        blank=True,
+    )
+
+    created_at=models.DateTimeField(
+        verbose_name='Fecha de Creación de archivo',
+        default=timezone.now,
+        )
+    
+    def __str__(self):
+        return self.file.name
