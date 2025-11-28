6d9599844be3a2c093d101eb12b2b00989fb541b
Guille <yoerguille@gmail.com>
2025-11-28 18:47:16 +0100

    Adding ClientContactRegisterView to the project

--- clients/views/clients_view.py
+++ clients/views/clients_view.py
@@ -9,10 +9,49 @@ class ClientRegisterView(CreateView):
     model = Client
     template_name = 'general/client_add.html'
     success_url= reverse_lazy('clients:clients_list')
-    form_class = AddClientForm
+    fields = [
+        'name',
+        'tipo',
+        'cif_nif',
+        't_number',
+        'email',
+        'direccion',
+        'other',
+        'created_at',
+    ]
 
     def form_valid(self, form):
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('clients:clients_detail', args=[self.object.pk])
+    
+class ClientContactRegisterView(CreateView):
+    context_object_name = 'contact'
+    model = ClientContact
+    template_name = 'general/client_contact_add.html'
+    fields = [
+        'client',
+        'first_name',
+        'last_name',
+        'cargo',
+        't_number',
+        'email',
+    ]
+
+    def form_valid(self, form):
+        # Guardar automáticamente el cliente correspondiente
+        form.instance.clients_id = self.kwargs["pk"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        # Volver al detalle del cliente
+        return reverse("clients:clients_detail", kwargs={"pk": self.kwargs["pk"]})
+    
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["client_pk"] = self.kwargs["pk"]
+        return context
 
 class ClientListView(ListView):
     model= Client
