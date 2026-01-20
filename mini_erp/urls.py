eef86807ef1ff911d1aaeb87afb35206b63a552f
Guille <yoerguille@gmail.com>
2026-01-20 22:57:41 +0100

    adding landing page

--- mini_erp/urls.py
+++ mini_erp/urls.py
@@ -4,11 +4,12 @@ from django.urls import path, include
 from .views import home_view
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import Login, logout_view, UserRegisterView
+from .views import Login, logout_view, UserRegisterView, landingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view.as_view(), name='home'),
+    path('', landingView.as_view(), name='landing' ),
+    path('home/', home_view.as_view(), name='home'),
     path('login/', Login.as_view(), name='login'),
     path("logout/", logout_view, name='logout'),
     path("register/", UserRegisterView.as_view(), name='register'),
